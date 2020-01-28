3252f07c8e18efcd6407e3411ecd42ebe07dbf95
Zprogrammer-code <skatboyza124@hotmail.com>
2020-01-28 13:24:12 -0700

    Done.

---
+++ db/migrate/20200128172257_create_pirates.rb
@@ -0,0 +1,10 @@
+class CreatePirates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pirates do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
